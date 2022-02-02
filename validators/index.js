cdbb25c558da38e2b3992d8a3694f81e79f34ddc
NjohPrince <jufredprince@gmail.com>
2022-02-02 13:21:59 +0100

    validators middleware

--- validators/index.js
+++ validators/index.js
@@ -1,26 +1,9 @@
-const { check } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-exports.userSignupValidator = [
-    check('fullname')
-        .not()
-        .isEmpty()
-        .withMessage("Your name is required."),
-    check('fullname')
-        .isLength({ min: 3 })
-        .withMessage("Name must be more than 2 characters long."),
-    check('email')
-        .isEmail()
-        .withMessage("Please enter a valid email."),
-    check('password')
-        .isLength({ min: 8 })
-        .withMessage("Password must be at least 8 characters long."),
-];
-
-exports.userSigninValidator = [
-    check('email')
-        .isEmail()
-        .withMessage("Invalid email address."),
-    check('password')
-        .isLength({ min: 8 })
-        .withMessage("Account passwords are 8+ characters long."),
-];
+exports.runValidators = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
+  next();
+};
