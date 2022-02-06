7ceb193aa5e2cdc3a55c9db45d72bc9589577cb6
NjohPrince <jufredprince@gmail.com>
2022-02-06 13:00:45 +0100

    updated signup validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth");
 
-router.post("/v1/signup", authController.signUpUser);
+const { userSignupValidator } = require("../validators/auth");
+const { runValidators } = require("../validators");
+
+router.post("/v1/signup", userSignupValidator, runValidators, authController.signUpUser);
 router.post("/v1/verify-otp", authController.verifyEmail);
 
 module.exports = router;
